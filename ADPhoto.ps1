ea175a91cceee3d2e44b0b03f4659bfd5ea67b90
uponom <uponomarenko@Y1.metinvest-smc.local>
2015-04-24 14:50:14 +0300

    ADPhotos v0.3
    First public release

---
+++ ADPhoto.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+
+
+.EXAMPLE
+
+
+
+
+if ($Install.IsPresent) {
+	Install-AsPSModule $($Overwrite.IsPresent)
+} elseif ($Path -eq "") {
+} else {
+	if (Test-Path $Path) {
+		$ReadOnlyMode = $WhatIf.IsPresent
+		if ($Force.IsPresent) {Write-Debug '"Force" switch exists. Exitsting photos will be overwrited'}
+	} else {
+	}
+}
+
